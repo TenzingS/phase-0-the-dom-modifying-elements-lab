1eb13ed2d88c147205ecf3355126a167bfa56128
TenzingS <thesherpatenzing@gmail.com>
2021-06-15 18:40:20 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory'
+
+victory.innerHTML = "<h1>YOUR-NAME is the champion.</h1>";
+
